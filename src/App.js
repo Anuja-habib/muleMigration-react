d91a73403be2a28eee558f0ff644dc1f4b1b5262
Anuja Habib (C) <anuja.habib@gm.com>
2025-01-31 14:24:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import FileUpload from "./components/file-upload/file-upload.component";
+import ColorSchemesExample from "./components/nav/nav"
+
+function App() {
+
+
+  return (
+  
+    
+    <div>
+      <ColorSchemesExample></ColorSchemesExample>
+      <FileUpload></FileUpload>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/file-upload/file-upload.component.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './/file-upload.component.css';
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      try {
+        const response = await fetch('/upload', { 
+          method: 'POST', 
+          body: formData 
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json(); 
+        console.log('File uploaded successfully:', data);
+
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
+    }
+  };
+
+  return (
+    <div className='file-upload-container'>
+      <div className="upload-card">
+      <div className="cloud-icon">
+      <label className='drag-drop-text'> Drag the file here</label>
+    <form onSubmit={handleSubmit}>
+      <input className='dummy'
+        label ="Drag or select the file"
+        type="file" 
+        onChange={handleFileChange} 
+      />
+      <button className='select-files-button' type="submit">Upload</button>
+    </form>
+    </div>
+    </div>
+    </div>
+    
+  );
+}
+
+export default FileUpload;
