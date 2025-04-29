d4f95f4d63dc9c5b8fa41d94384a4461dd869f0a
Anuja Habib (C) <anuja.habib@gm.com>
2025-04-29 11:10:39 +0530

    Update config.jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,6 @@
 import { react } from 'react'
 
 const config = {
-  apiUrlPython: 'http://127.0.0.1:5004',
+  apiUrlPython: 'https://migration-accelerator-server-a32399b22dff.herokuapp.com/',
 }
 export default config
